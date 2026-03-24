35fe588edde9294ff95856690846b54cca3bbb79
timo0n22 <akkdlyaandroid@gmail.com>
2026-03-24 20:28:22 +0600

    add autostart and safe battery

--- nixos/modules/apple-silicon.nix
+++ nixos/modules/apple-silicon.nix
@@ -28,4 +28,14 @@
       WIFI_PWR_ON_BAT             = "off";   # wifi powersave уже отключён в net.nix
     };
   };
+  systemd.services.battery-charge-threshold = {
+    description   = "Set battery charge threshold to 80%";
+    wantedBy      = [ "multi-user.target" ];
+    after         = [ "multi-user.target" ];
+    serviceConfig = {
+      Type      = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "/bin/sh -c 'echo 80 > /sys/class/power_supply/macsmc-battery/charge_control_end_threshold'";
+    };
+  };
 }
--- nixos/modules/user.nix
+++ nixos/modules/user.nix
@@ -9,8 +9,15 @@
     };
   };
 
-  # Автологин на tty1 — niri стартует из fish
-  services.getty.autologinUser = user;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd niri-session";
+        user    = "greeter";
+      };
+    };
+  };
 
   # wireshark без sudo
   programs.wireshark.enable = true;
